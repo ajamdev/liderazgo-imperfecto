afa90bbe2632ccbdac86c45d4afd792147452e97
Angelo Almeida <angelo94.aa@gmail.com>
2025-11-23 08:47:24 -0500

    Agregar tipo y interfaz para los servicios disponibles

---
+++ src/lib/type/services.ts
@@ -0,0 +1,15 @@
+type serviceId =
+  | 'liderazgo_imperfecto'
+  | 'se_parte_de_nuestra_comunidad'
+  | 'tu_companero_de_crecimiento'
+  | 'la_biblioteca_del_lider'
+  | 'descubre_tu_proximo_aprendizaje'
+  | 'jose_en_accion'
+
+  export interface Service {
+    id: serviceId;
+    name: string;
+    description: string;
+    link: string;
+    active: boolean;
+  }
