dd1aaf504e681ca455576891c4d9203ca128d335
Angelo Almeida <angelo94.aa@gmail.com>
2025-11-24 10:26:54 -0500

    fix: update header script type and modify job field to accept an array

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from "astro:content";
 const profile = defineCollection({
   schema: z.object({
     title: z.string(),
-    job: z.string(),
+    job: z.string().array(),
     link: z.object({
       linkedin: z.string().url().optional(),
       youtube: z.string().url().optional(),
