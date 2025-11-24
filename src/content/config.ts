5a25002b467b63ae14fc08f2aea1b012d1ce3468
Angelo Almeida <angelo94.aa@gmail.com>
2025-11-24 10:19:45 -0500

    Agregar colección de perfiles en la configuración

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+
+const profile = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    job: z.string(),
+    link: z.object({
+      linkedin: z.string().url().optional(),
+      youtube: z.string().url().optional(),
+    }),
+    description: z.string(),
+    image: z.string().url()
+  })
+})
+
+export const collections = {
+  profile
+};
