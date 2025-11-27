b647a304cb66dd1a4d388f4c935a581a4b01c715
Angelo Almeida <angelo94.aa@gmail.com>
2025-11-27 18:07:18 -0500

    feat: eliminar configuración de salida del servidor en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import node from '@astrojs/node';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
