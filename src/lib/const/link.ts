c966bfebba5080fce4a34d54651d9198ef03b6d5
Angelo Almeida <angelo94.aa@gmail.com>
2025-11-28 10:25:28 -0500

    feat: actualizar estado activo y agregar íconos a los enlaces de navegación

--- src/lib/const/link.ts
+++ src/lib/const/link.ts
@@ -1,29 +1,49 @@
 import type { Link } from "../type/links";
+import Mentoria from "@/assets/icons/mentoria.svg"
+import Consultorias from "@/assets/icons/consultoria.svg"
+import Coaching from "@/assets/icons/conferencias.svg"
+import Llamada from "@/assets/icons/phone.svg"
+import Workshops from "@/assets/icons/shield.svg"
 
 export const LINK: Link[] = [
   {
     link: "/mentoria-ejecutiva",
     title: "Mentoría Ejecutiva",
-    active: false,
+    active: true,
+    icon:{
+      name: Mentoria
+    }
   },
   {
     link: "/consultorias",
     title: "Consultorías",
     active: false,
+    icon: {
+      name: Consultorias  
+    }
   },
   {
     link: "coaching-ejecutivo-y-alineacion-de-equipo",
     title: "Coaching Ejecutivo y Alineación de Equipo",
     active: false,
+    icon: {
+      name: Coaching
+    }
   },
   {
     link: "/Llamada-de-emergencia",
     title: "Llamada de Emergencia",
     active: false,
+    icon: {
+      name: Llamada
+    }
   },
   {
     link: "/workshops",
     title: "Workshops sobre Seguridad Psicológica para la transformación",
     active: false,
+    icon: {
+      name: Workshops
+    }
   }
 ] as const;
