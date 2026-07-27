5698a02932eadfb8ea4021b6afea12f0503238e8
Angelo Almeida <angelo94.aa@gmail.com>
2026-07-26 22:54:42 -0500

    Reformatear configuración de Astro para mejorar la legibilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	site: 'https://liderazgoimperfecto.com',
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
