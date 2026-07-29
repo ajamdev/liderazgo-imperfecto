2aead9ac5b878f36f8f0a8098dbc74463d8806fb
Angelo Almeida <angelo94.aa@gmail.com>
2026-07-29 11:46:09 -0500

    Eliminar elementos obsoletos del programa para mejorar la claridad del contenido

--- src/lib/const/program.ts
+++ src/lib/const/program.ts
@@ -4,8 +4,6 @@ export const PROGRAM = [
 	{ answer: 'Evento de apertura y cierre + badges ' },
 	{ answer: '5 ciclos de aprendizaje' },
 	{ answer: 'Contenidos asíncronos en HackU' },
-	{ answer: 'Comunidad de líderes' },
-	{ answer: 'Conservatorios con Jose Echeverri ' },
 	{ answer: '5 sesiones de acompañamiento individual' },
 	{ answer: 'Informe organizacional' }
 ]
